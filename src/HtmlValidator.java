775af61627bb5ed9f67461b9950a9a2a02e73d4f
Nadia Martinez <nadiaz.martinez@calbaptist.edu>
2022-02-09 20:16:30 -0800

    “Pfinal commit

--- src/HtmlValidator.java
+++ src/HtmlValidator.java
@@ -6,58 +6,60 @@ import java.util.*;
  */
 
 public class HtmlValidator {
-    private Queue<HtmlTag> tags;
+    private Queue<HtmlTag> tags; //default constructor
 
-    public HtmlValidator() {
+    //empty queue (Linked list) stores HTML tags
+    public HtmlValidator() { //first constructor
 
-        tags = new LinkedList<>();
+        tags = new LinkedList<>(); //initializes empty queue
     }
 
-    public HtmlValidator(Queue<HtmlTag> tags) {
+    //initializes validator with a separate copy of the queue that was passed in
+    public HtmlValidator(Queue<HtmlTag> tags) { //second constructor
         if (tags == null) {
-            throw new IllegalArgumentException("Initial tags cannot be null.");
+            throw new IllegalArgumentException("Initial tags cannot be null."); //error message
         }
 
-        //do I need line 22??
-        tags = copy(tags);
-        this.tags = new LinkedList<>(tags);
+        this.tags = new LinkedList<>(tags); //initializes queue with input from parameters
     }
 
     //adds the given tag to end of validator's queue
     //if null throws exception
     public void addTag(HtmlTag tag) {
         if(tag == null) {
-            throw new IllegalArgumentException("Initial tags cannot be null");
+            throw new IllegalArgumentException("Initial tags cannot be null"); //error message for empty tag
         }
         tags.add(tag);
     }
 
     //returns validator's queue of HTML tags
-    public Queue<HtmlTag> getTags() {
+    public Queue<HtmlTag> getTags() { //queue contains all tags + reflects any changes
 
         return copy(tags);
     }
 
 
-    //remove all tags matching element
+    //remove all tags with string element
     public void removeAll(String element) {
         if (element == null) throw new IllegalArgumentException();
         tags.removeIf(tag -> tag.getElement().equalsIgnoreCase(element));
     }
 
+    //prints indented text representation of the HTML tags in the queue and validates HTML file
+    //calls helper method printTag
     public void validate(){
-        Queue<HtmlTag>copyQ = copy(tags);
-        Stack<HtmlTag> tagsStack = new Stack<HtmlTag>();
+        Queue<HtmlTag>copyQ = copy(tags); //copy of queue for HTML file input
+        Stack<HtmlTag> tagsStack = new Stack<HtmlTag>(); //analyzes queue
         int indent = 0;
         while (!copyQ.isEmpty()) {
             HtmlTag t = copyQ.remove();
             if(!t.isOpenTag()) {
                 if (!tagsStack.isEmpty() && t.matches(tagsStack.peek())) {
                     indent--;
-                    printTag(indent,t);
+                    printTag(indent,t); //prints indentation + tag
                     tagsStack.pop();
                 } else{
-                    System.out.println("ERROR unexpected tag:" + t);
+                    System.out.println("ERROR unexpected tag:" + t); //error message
                 }
             } else {
                 printTag(indent, t);
@@ -69,10 +71,13 @@ public class HtmlValidator {
         }
         while(!tagsStack.isEmpty()){
             HtmlTag tag = tagsStack.pop();
-            System.out.println("ERROR unclosed tag"+ tag);
+            System.out.println("ERROR unclosed tag"+ tag); //error for unclosed tag
 
         }
     }
+
+    //helper method
+    //evaluates tags returns copy of queue or null
     private Queue<HtmlTag> copy(Queue<HtmlTag>tags){
         if(tags != null){
             Queue<HtmlTag> copyQ = new LinkedList<HtmlTag>();
@@ -87,7 +92,9 @@ public class HtmlValidator {
         return null;
     }
 
-    private void printTag(int n, HtmlTag tag) {
+    //helper method
+    //prints indentation then tag
+    private void printTag(int n, HtmlTag tag) {  //two constructors
         for (int i = 0; i < n; i++) {
             System.out.print(" ");
         }
--- tst/HtmlValidatorTest.java
+++ tst/HtmlValidatorTest.java
@@ -7,13 +7,19 @@ import java.util.*;
 
 public class HtmlValidatorTest {
 
+    //files used to test against
     private static final String EXPECTED_TEMPLATE = "expected_output/validate_result_for_test%d.txt";
     private static final String INPUT_TEMPLATE = "expected_output/test%d.html";
 
+
+    //calls helper method testValidatorWithFiles
+    //uses Expected template + input template for each individual test 1-8
     private static void testAgainstFiles(int testNumber) {
         testValidatorWithFiles(String.format(EXPECTED_TEMPLATE, testNumber), String.format(INPUT_TEMPLATE, testNumber));
     }
 
+    //helper method
+    //uses files to test input
     private static void testValidatorWithFiles(String expectedOutputFilePath, String validatorInputFilePath) {
         String rawInput = dumpFileContentsToString(validatorInputFilePath);
         String expected = dumpFileContentsToString(expectedOutputFilePath);
@@ -21,9 +27,10 @@ public class HtmlValidatorTest {
 
         String validatorOutput = captureValidatorOutput(validator);
 
-        Assert.assertEquals("Validator output must match expected value", expected, validatorOutput);
+        Assert.assertEquals("Validator output must match expected value", expected, validatorOutput);  //error message
     }
 
+    //tests and returns String of HtmlValidator validator output
     private static String captureValidatorOutput(HtmlValidator validator) {
         ByteArrayOutputStream baos = new ByteArrayOutputStream();
         PrintStream ps = new PrintStream(baos);
@@ -37,11 +44,12 @@ public class HtmlValidatorTest {
         return baos.toString();
     }
 
+    //returns string of parameters or error message + null
     private static String dumpFileContentsToString(String filePath) {
         try {
             return new String(Files.readAllBytes(Paths.get(filePath)), StandardCharsets.UTF_8);
         } catch (IOException e) {
-            Assert.fail("Could not load file: " + filePath);
+            Assert.fail("Could not load file: " + filePath); //error message
             return null;
         }
     }
@@ -86,6 +94,7 @@ public class HtmlValidatorTest {
         testAgainstFiles(8);
     }
 
+    //
     @Test
     public void addTagTest() {
         HtmlTag[] tagsArr = {new HtmlTag("Hello"), new HtmlTag("There")};
@@ -95,7 +104,7 @@ public class HtmlValidatorTest {
         tags.forEach(validator::addTag);
 
         // TODO: Write the expect assert statement for getting the correct tags.
-        Assert.assertEquals(tags,validator.getTags());
+        Assert.assertEquals(tags,validator.getTags());   //
 
 
     }
@@ -103,26 +112,29 @@ public class HtmlValidatorTest {
 
     @Test(expected = IllegalArgumentException.class)
     // TODO: write the method addNullTagTest to account null tags.
+
+    //adds up all null tags
     public void addNullTagTest() {
         HtmlValidator validator = new HtmlValidator();
         validator.addTag(null);
     }
 
+
     @Test
-    public void removeAllTest() {
+    public void removeAllTest() { //given
         HtmlTag[] tagsArr = {new HtmlTag("Hello"), new HtmlTag("There")};
         List<HtmlTag> tags = new ArrayList<>(Arrays.asList(tagsArr));
         HtmlValidator validator = new HtmlValidator();
         tags.forEach(validator::addTag);
 
         // TODO: Incorporate the addition and removal of tags
-        validator.addTag(new HtmlTag("General Kenobi"));
+        validator.addTag(new HtmlTag("General Kenobi"));  //adds tags
 
-        validator.removeAll("General Kenobi");
+        validator.removeAll("General Kenobi"); //removes tags
 
 
         // TODO: Write the correct assert statement to test the tags
-        Assert.assertEquals(tags, validator.getTags());
+        Assert.assertEquals(tags, validator.getTags());   //test tags with validator tags
 
     }
 
@@ -133,7 +145,7 @@ public class HtmlValidatorTest {
     public void removeAllNullTest() {
         HtmlValidator validator = new HtmlValidator();
 
-        validator.removeAll(null);
+        validator.removeAll(null);  //removes all null elements
     }
 }
 
